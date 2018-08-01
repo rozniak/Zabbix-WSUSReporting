b708876cf7d934582aa7776fcd98622cef99253d
Rory Fewell <roryfewell@hotmail.com>
2018-08-01 10:30:40 +0100

    Add script for totaling the amount of computers that haven't reported in within 30 days

---
+++ Get-WsusOldComputerCount.ps1
@@ -0,0 +1,23 @@
+﻿<#
+    .SYNOPSIS
+    This script is used for obtaining the count of computers that have not reported
+    into WSUS in the last 30 days.
+
+    .DESCRIPTION
+    This script is extremely simple as it is simply used to return a standard integer
+    value that represents the count of computers that have not reported into this WSUS
+    server within the last 30 days.
+    
+    .EXAMPLE
+    Get-WsusOldComputerCount.ps1
+
+    .NOTES
+    Author: Rory Fewell
+    GitHub: https://github.com/rozniak
+    Website: https://oddmatics.uk
+#>
+
+$cutoffDateTime = [System.DateTime]::UtcNow.AddDays(-30)
+$oldComputers = Get-WsusComputer | Where-Object { $_.LastReportedStatusTime -le $cutoffDateTime }
+
+return $oldComputers.Length
