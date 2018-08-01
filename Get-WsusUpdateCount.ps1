b30e555e7204b8788550a19786323bfd08f112af
Rory Fewell <roryfewell@hotmail.com>
2018-08-01 14:42:58 +0100

    Improve update count script so that it uses the WSUS API rather than PS module - commands now much quicker and report in Zabbix without timeouts

--- Get-WsusUpdateCount.ps1
+++ Get-WsusUpdateCount.ps1
@@ -6,13 +6,13 @@
     This script is extremely simple as it is simply used to return a standard integer
     value that represents the count of updates matching the specified criteria.
 
-    .PARAMETER UpdateApproval
+    .PARAMETER approval
     The approval state of updates to count. (Choices: AnyExceptDeclined,
                                                       Approved,
                                                       Declined,
                                                       Unapproved)
 
-    .PARAMETER UpdateStatus
+    .PARAMETER status
     The status of updates to count. (Choices: Any,
                                               Failed,
                                               FailedOrNeeded,
@@ -32,11 +32,105 @@
 
 Param (
     [Parameter(Position=0, Mandatory=$TRUE)]
+    [String]
     $UpdateApproval,
     [Parameter(Position=1, Mandatory=$TRUE)]
+    [String]
     $UpdateStatus
 )
 
-$updates = Get-WsusUpdate -Approval $UpdateApproval -Status $UpdateStatus
+# Load WSUS Administration library
+#
+$assemblies = Get-ChildItem -Path "C:\Windows\Microsoft.NET\assembly\GAC_MSIL\Microsoft.UpdateServices.Administration" | Get-ChildItem
+$dllTarget = $assemblies[0].FullName
 
-return $updates.Length
+[void][Reflection.Assembly]::LoadFrom($dllTarget)
+
+# Get WSUS server connection instance
+#
+$wsusServer = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer()
+
+# Retrieve the updates
+#
+$scope = New-Object -TypeName Microsoft.UpdateServices.Administration.UpdateScope
+
+# Set up the approval scope
+#
+switch ($UpdateApproval)
+{
+    "AnyExceptDeclined" {
+        $scope.ApprovedStates =
+            [Microsoft.UpdateServices.Administration.ApprovedStates]::HasStaleUpdateApprovals -bor
+            [Microsoft.UpdateServices.Administration.ApprovedStates]::LatestRevisionApproved -bor
+            [Microsoft.UpdateServices.Administration.ApprovedStates]::NotApproved;
+    }
+
+    "Approved" {
+        $scope.ApprovedStates =
+            [Microsoft.UpdateServices.Administration.ApprovedStates]::HasStaleUpdateApprovals -bor
+            [Microsoft.UpdateServices.Administration.ApprovedStates]::LatestRevisionApproved;
+    }
+
+    "Declined" {
+        $scope.ApprovedStates = [Microsoft.UpdateServices.Administration.ApprovedStates]::Declined
+    }
+
+    "Unapproved" {
+        $scope.ApprovedStates = [Microsoft.UpdateServices.Administration.ApprovedStates]::NotApproved
+    }
+
+    default {
+        return -1
+    }
+}
+
+# Set up the installation status report scope
+#
+switch ($UpdateStatus)
+{
+    "Any" {
+        $scope.IncludedInstallationStates = [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::All
+    }
+
+    "Failed" {
+        $scope.IncludedInstallationStates = [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::Failed
+    }
+
+    "FailedOrNeeded" {
+        $scope.IncludedInstallationStates =
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::Failed -bor
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::NotInstalled;
+            #[Microsoft.UpdateServices.Administration.UpdateInstallationStates]::InstalledPendingReboot;
+    }
+
+    "InstalledOrNotApplicable" {
+        $scope.IncludedInstallationStates =
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::Installed -bor
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::NotApplicable;
+    }
+
+    "InstalledOrNotApplicableOrNoStatus" {
+        $scope.IncludedInstallationStates =
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::Installed -bor
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::NotApplicable -bor
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::Unknown;
+    }
+
+    "Needed" {
+        $scope.IncludedInstallationStates =
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::NotInstalled -bor
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::InstalledPendingReboot;
+        $scope.ExcludedInstallationStates =
+            [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::Failed;
+    }
+
+    "NoStatus" {
+        $scope.IncludedInstallationStates = [Microsoft.UpdateServices.Administration.UpdateInstallationStates]::Unknown
+    }
+
+    default {
+        return -1
+    }
+}
+
+return $wsusServer.GetUpdateCount($scope);
